b0b34f31252e4929332279072a2f922c5b668a15
rafael <rafael@rafaelu-24.04>
2024-12-05 21:24:04 -0300

    Teste com banco de dados

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,35 @@
-import database from "../../../../infra/database";
+import database from "/infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  //console.log(result.rows);
-  response.status(200).json({ chave: "success" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionResult = await database.query(
+    "SHOW max_connections;",
+  );
+
+  const databaseMaxConnectionValue = parseInt(
+    databaseMaxConnectionResult.rows[0].max_connections,
+  );
+
+  const databaseActivityConnectionResult = await database.query(
+    "SELECT * FROM pg_stat_activity WHERE datname = 'local_db';",
+  );
+  console.log(databaseActivityConnectionResult.rows);
+  const databaseActivityConnectionCount =
+    databaseActivityConnectionResult.rows.length;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: databaseMaxConnectionValue,
+      },
+    },
+  });
 }
 
 export default status;
